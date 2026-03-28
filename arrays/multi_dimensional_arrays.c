ef36a75355cb1c30fec193f929c7ebd11df2ffd6
thiveth <59804297+thiveth@users.noreply.github.com>
2026-03-28 19:12:47 -0400

    Update multi_dimensional_arrays.c
    Now able to input multiple arrays instead of a set number

--- arrays/multi_dimensional_arrays.c
+++ arrays/multi_dimensional_arrays.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
-void combineArrays(int *arr1, int*arr2, int tSize, int result[][tSize]){
+void combineArrays(int *arrays[], int numArrays, int tSize, int result[][tSize]){
 
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < tSize; j++){
-            if(i==0){
-                result[i][j] = arr1[j];
-            }
-            else {
-                result[i][j] = arr2[j];
-            }
+   for (int i = 0; i < numArrays; i++){
+            for(int j = 0; j < tSize; j++){
+                result[i][j] = arrays[i][j];
         }
-    }
+   }
 
 }
 
@@ -21,27 +16,30 @@ int main(){
 
     int physicsGrades[] = {87, 92, 95, 74, 68};
     int mathGrades[] = {63, 98, 99, 75, 79};
+    int biologyGrades[] = {58, 98, 100, 62, 74};
+
     int tSize = sizeof(physicsGrades) / sizeof(physicsGrades[0]);
 
-    // int *arrays[] = {physicsGrades, mathGrades};
-
-    // Want
-    /*int combinedArray[2][5] = {{87, 92, 95, 74, 68},
-                                {63, 98, 99, 75, 79}}*/
-
-    int combinedArray[2][tSize];
+    int *arrays[] = {physicsGrades, mathGrades, biologyGrades};
+    int numArrays =  sizeof(arrays) / sizeof(arrays[0]); // size of a pointer = 8 bytes so 16 / 8 = 2 or num of arrays 
 
 
-    combineArrays(physicsGrades, mathGrades, tSize, combinedArray);
+    int combinedArray[numArrays][tSize];
+    
 
-    for(int i = 0; i < 2; i++){
+    combineArrays(arrays, numArrays, tSize, combinedArray);
+
+    for(int i = 0; i < numArrays; i++){
         for(int j = 0; j < tSize; j++){
             printf("%d", combinedArray[i][j]);
-
+            
+            // Print Formatting
             if (j < tSize - 1){
                 printf(", ");
             }
         }
+        
+        // Print new line after each iteration
         printf("\n");
     }
 
